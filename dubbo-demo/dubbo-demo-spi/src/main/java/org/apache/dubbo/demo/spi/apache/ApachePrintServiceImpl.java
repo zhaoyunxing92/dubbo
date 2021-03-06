59409f6f2c57adf8892028718449b3a909f9326e
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2021-03-06 20:10:16 +0800

    add:spi

--- dubbo-demo/dubbo-demo-spi/src/main/java/org/apache/dubbo/demo/spi/apache/ApachePrintServiceImpl.java
+++ dubbo-demo/dubbo-demo-spi/src/main/java/org/apache/dubbo/demo/spi/apache/ApachePrintServiceImpl.java
@@ -6,8 +6,19 @@ package org.apache.dubbo.demo.spi.apache;
  */
 public class ApachePrintServiceImpl implements ApachePrintService {
 
+    /**
+     * 通过setter 注入服务
+     *
+     * @param helloService hello服务
+     */
+    public void setHelloService(HelloService helloService) {
+        helloService.echo("abc");
+    }
+
     @Override
     public void printInfo() {
         System.out.println("hello apache dubbo spi");
     }
+
+
 }
